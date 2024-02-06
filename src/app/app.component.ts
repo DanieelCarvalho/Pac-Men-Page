6ec362d36330ffc734c974d87915aef5986454e5
Daniel Carvalho <Danielscg2012@gmail.com>
2024-02-06 13:29:39 -0300

    add components header e footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,21 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+  ],
 })
 export class AppComponent {
   title = 'PacMan-Page';
